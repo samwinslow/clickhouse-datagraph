350c482d322a6d73b13b4fa7b4dec3b647d04e1f
Sam Winslow <sammywinslow@gmail.com>
2022-02-10 20:15:03 -0500

    add addtl. schemas

--- aviation.schema.sql
+++ aviation.schema.sql
@@ -36,3 +36,77 @@ CREATE TABLE aviation.airline_accidents
 ENGINE = MergeTree()
 ORDER BY sipHash64(EventId)
 SAMPLE BY sipHash64(EventId);
+
+CREATE TABLE aviation.faa_incidents_data
+(
+  `AIDSReportNumber` String,
+  `LocalEventDate` Date,
+  `EventCity` Nullable(String),
+  `EventState` Nullable(String),
+  `EventAirport` Nullable(String),
+  `EventType` String,
+  `AircraftDamage` Nullable(String),
+  `FlightPhase` Nullable(String),
+  `AircraftMake` Nullable(String),
+  `AircraftModel` Nullable(String),
+  `AircraftSeries` Nullable(String),
+  `Operator` Nullable(String),
+  `PrimaryFlightType` Nullable(String),
+  `FlightConductCode` Nullable(String),
+  `FlightPlanFiledCode` Nullable(String),
+  `AircraftRegistrationNbr` String,
+  `TotalFatalities` UInt16,
+  `TotalInjuries` UInt16,
+  `AircraftEngineMake` Nullable(String),
+  `AircraftEngineModel` Nullable(String),
+  `EngineGroupCode` Nullable(String),
+  `NbrofEngines` Nullable(UInt8),
+  `PICCertificateType` Nullable(String),
+  `PICFlightTimeTotalHrs` Nullable(Float64),
+  `PICFlightTimeTotalMake-Model` Nullable(Float64),
+  `.1` Nullable(Float64)
+)
+ENGINE = MergeTree()
+ORDER BY sipHash64(AIDSReportNumber)
+SAMPLE BY sipHash64(AIDSReportNumber);
+
+CREATE TABLE aviation.ntsb_aviation_data
+(
+  `NTSB_RPRT_NBR` String,
+  `ACFT_REGIST_NBR` Nullable(String),
+  `ACFT_SERIAL_NBR` Nullable(String),
+  `EV_TYPE_DESC` String,
+  `EVENT_LCL_DATE` Date,
+  `LOC_STATE_CODE_STD` Nullable(String),
+  `ARPT_NAME_STD` Nullable(String),
+  `FLTCNDCT_DESC` Nullable(String),
+  `OPRTR_SCHED_DESC` Nullable(String),
+  `OPRTR_NSDC_NAME_STD` Nullable(String),
+  `ACFT_NSDC_MAKE_STD` Nullable(String),
+  `ACFT_NSDC_MODEL_STD` Nullable(String),
+  `ACFT_NSDC_SERIES_STD` Nullable(String),
+  `REPORT_STATUS` Nullable(String),
+  `INJURY_DESC` Nullable(String),
+  `FLIGHT_PHASE_DESC` Nullable(String)
+)
+ENGINE = MergeTree()
+ORDER BY sipHash64(NTSB_RPRT_NBR)
+SAMPLE BY sipHash64(NTSB_RPRT_NBR);
+
+CREATE TABLE aviation.world_aircraft_accident_summary
+(
+  `WAASSubsetEventId` String,
+  `LocalEventDate` Date,
+  `Aircraft` String,
+  `AircraftOperator` String,
+  `EventLocation` String,
+  `CrewFatalities` UInt16,
+  `CrewInjured` UInt16,
+  `CrewAboard` UInt16,
+  `PAXFatalities` UInt16,
+  `PAXInjuries` UInt16,
+  `PAXAboard` UInt16
+)
+ENGINE = MergeTree()
+ORDER BY sipHash64(WAASSubsetEventId)
+SAMPLE BY sipHash64(WAASSubsetEventId);
--- scripts/aggregate_stats.py
+++ scripts/aggregate_stats.py
@@ -1,3 +1,4 @@
+import pandas
 from utils import read_dataset
 
 
@@ -14,4 +15,8 @@ for dataset in datasets:
   source_path = '/'.join([base_dir, dataset])
   df = read_dataset(source_path)
   print('Count of null rows by column:')
-  print(df.isnull().sum(), '\n\n')
+  print(df.isnull().sum(), '\n')
+
+  print(f'Head of {dataset}:')
+  pandas.set_option('display.max_columns', None)
+  print(df.head(10), '\n\n')
